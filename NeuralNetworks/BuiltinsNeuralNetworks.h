8049ec1a6728aa4a405e803983753a1ebfbadde7
Alex375 <alexandre.josien@gmail.com>
2020-10-15 23:48:18 +0200

    added neural complete neural network (not working)

--- NeuralNetworks/BuiltinsNeuralNetworks.h
+++ NeuralNetworks/BuiltinsNeuralNetworks.h
@@ -5,5 +5,6 @@ double sigmoideDerivate(double x) ;
 double sigmoid(double x);
 double randd();
 int lengthl(double * l[]);
+void printNetworks(double *h[6], double *z[8]);
 
 #endif
--- NeuralNetworks/NeuralNetwork.c
+++ NeuralNetworks/NeuralNetwork.c
@@ -8,8 +8,11 @@
 #define OUTPUT_NUMEBR = 1
 
 void NeuraleNetwork() ;
-double update(double *n[]);
+double update(double *n[], int size);
 double delta(double error, double out);
+double deltaHiden(double out, double outi, double dk, double w);
+void upadateTree(double *h[6], double *z[8]);
+void learn(double x, double y, double activationH, double activationZ, double fWeight[3], double iWeight[2], double excpect, double hb, double hz);
 
 int main()
 {
@@ -21,50 +24,74 @@ void NeuraleNetwork()
     double excpect = 1;
     double x = 1;
     double y = 0;
-    double iWeight[2] = {0, 0};
-    double fWeight[3] = {0, 0, 0};
+    double iWeight[2] = {0.5, 0.5};
+    double fWeight[3] = {0.5, 0.5, 0.5};
     double activationH = 0;
-    double bh = 0;
+    double bh = 1;
     double activationZ = 0;
-    double bz = 0;
+    double bz = 1;
+
 
     double *h[6] = {&activationH, &bh, &x, &iWeight[0], &y, &iWeight[1]};
     double *z[8] = {&activationZ, &bz, &x, &fWeight[0], &activationH, &fWeight[1], &y, &fWeight[2]};
-
-    //Back prop
-
-    //#2
-    activationH = update(h);
-    activationZ = update(z);
-    printf("h = %f\nz = %f", activationH, activationZ);
-    //#3
-    double error = (excpect - activationZ) * sigmoideDerivate(activationZ);
-
-    //#4
-    fWeight[0] += delta(error, x);
-    fWeight[1] += delta(error, activationH);
-    fWeight[2] += delta(error, y);
+    printf("Random synaptic weight : ");
+    printNetworks(h, z);
 
 
+    for (int i = 0; i < 10000; i++)
+    {
+        x = 1;
+        y = 0;
+        excpect = 1;
+        upadateTree(h, z);
+        learn(x, y, activationH, activationZ, fWeight, iWeight, excpect, bh, bz);
+        x = 1;
+        y = 1;
+        excpect = 0;
+        upadateTree(h, z);
+        learn(x, y, activationH, activationZ, fWeight, iWeight, excpect, bh, bz);
+        x = 0;
+        y = 1;
+        excpect = 1;
+        upadateTree(h, z);
+        learn(x, y, activationH, activationZ, fWeight, iWeight, excpect, bh, bz);
+        x = 0;
+        y = 0;
+        excpect = 0;
+        upadateTree(h, z);
+        learn(x, y, activationH, activationZ, fWeight, iWeight, excpect, bh, bz);
+    }
 
 
-
-
+    printNetworks(h, z);
+    upadateTree(h, z);
+    printf("Res for 1, 1 = %f\n", activationZ);
+    x = 0;
+    upadateTree(h, z);
+    printf("Res for 0, 1 = %f\n", activationZ);
+    y = 0;
+    upadateTree(h, z);
+    printf("Res for 0, 0 = %f\n", activationZ);
+    x = 1;
+    upadateTree(h, z);
+    printf("Res for 1, 0 = %f\n", activationZ);
 }
 
 double delta(double error, double out)
 {
-    double v = 0.1;
+    double v = 1;
     return error * out * v;
 }
 
-double deltaHiden(double error, double out)
+double deltaHiden(double out, double outi, double dk, double w)
 {
-    return error * out * v;
+    double v = 1;
+    double d = sigmoideDerivate(out) * dk * w;
+    return v * d * outi;
 }
 
 
-double update(double *n[], double size)
+double update(double *n[], int size)
 {
     double res = *n[1];
     // size_t s = sizeof(n) / sizeof(n[0]);
@@ -76,6 +103,41 @@ double update(double *n[], double size)
     return sigmoid(res);
 }
 
+
+void upadateTree(double *h[6], double *z[8])
+{
+    *h[0] = update(h, 6);
+    *z[0] = update(z, 8);
+}
+
+void learn(double x, double y, double activationH, double activationZ, double fWeight[3], double iWeight[2], double excpect, double hb, double hz)
+{
+//#3
+    double error = (excpect - activationZ) * sigmoideDerivate(activationZ);
+
+    //#4
+
+    // double deltaF0 = delta(error, x);
+    // double deltaF1 = delta(error, activationH);
+    // double deltaF2 = delta(error, y);
+
+    fWeight[0] += delta(error, x);
+    fWeight[1] += delta(error, activationH);
+    fWeight[2] += delta(error, y);
+    // double deltaBH = deltaHiden(1, activationZ, error, hz);
+
+    hz += deltaHiden(1, activationZ, error, hz);
+
+    iWeight[0] += deltaHiden(activationH, x, error, iWeight[0]);
+    iWeight[1] += deltaHiden(activationH, y, error, iWeight[1]);
+    hb += deltaHiden(1, activationH, error, hb);
+
+    // fWeight[0] += deltaF0;
+    // fWeight[1] += deltaF1;
+    // fWeight[2] += deltaF2;
+
+    // hz += deltaBH;
+}
 
 //     NeuralNetworks();
 // }
